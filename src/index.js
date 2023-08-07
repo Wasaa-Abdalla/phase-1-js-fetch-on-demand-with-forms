391b5a0351d881507eebc30613956354e972a0bd
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-08-07 21:51:01 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const input = document.querySelector('input#searchByID')
+    console.log(input.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const title = document.querySelector('h4')
+        const summary = document.querySelector('p')
+        title.innerText = data.title
+        summary.innerText = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
